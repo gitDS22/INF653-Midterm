b793efc0f10375b9f6d3efc4ccbea5180c63a76a
David <d_schiffer@mail.fhsu.edu>
2022-03-18 15:21:06 -0500

    testing

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -42,6 +42,7 @@
     echo var_dump($quote_object->categoryId);
     //Create the post
     if($quote_object->create()) {
+        echo var_dump($quote_object->id);
         $quote_item = array(
             'id' => $quote_object->id,
             'quote' => $quote_object->quote,
